39181641b6c9b30926e21c1b8a89c6ec914de089
Tulza <40633228+tulza@users.noreply.github.com>
2024-02-29 20:43:23 +1300

    init new template

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://tulza.github.io/tip-calculator-app-main",
-  plugins: [react()],
-});
